532259494e08a8ee80c81efa70c63376094d0964
jinparksj <jinparksj@gmail.com>
2019-01-09 15:33:21 -0800

    done fore readNPYheader

--- readNPYheader.py
+++ readNPYheader.py
@@ -1,25 +1,65 @@
 import numpy as np
-import os
-import sys
-import cv2
+import re
 
 
 
-# def fxn_readNPYheader(filename):
-filename = "data/lab_rgb.npy"
-fid = np.load(filename)
+def readNPYheader(filename):
+    filename = "data/chair_bed_rgb.npy"
 
-if (fid.size == -1):
-    if len(os.path.dirname(filename)):
-        print('Permission Denied: %s' % (filename))
-        # return None
-    else:
-        print('File not found: %s' % (filename))
-        # return None
+    with open(filename, 'rb') as f:
+        try:
+            dtypesPYTHON = np.array(['uint8', 'uint16', 'uint32', 'uint64', 'int8', 'int16', 'int32', 'int64', 'single',
+                                     'double', 'logical'])
+            dtypesNPY = np.array(['u1', 'u2', 'u4', 'u8', 'i1', 'i2', 'i4', 'i8', 'f4', 'f8', 'b1'])
+            farray = np.fromfile(f, count=6 * 1, dtype='uint8')
+            magicString = farray.reshape((6,))
 
-try:
-    dtypesPYTHON = ['uint8', ]
+            if not np.all(magicString == np.array([147, 78, 85, 77, 80, 89])):
+                print('readNPY:NotNUMPYFile, Error: This file does not appear to be NUMPY format based on the header.')
+                raise AssertionError()
 
+            majorVersion = np.fromfile(f, count=1 * 1, dtype='uint8')
+            majorVersion = majorVersion.reshape((1,))
+
+            minorVersion = np.fromfile(f, count=1 * 1, dtype='uint8')
+            minorVersion = minorVersion.reshape((1,))
+
+            npyVersion = [majorVersion[0], minorVersion[0]]
+
+            headerLength = np.fromfile(f, count=1 * 1, dtype='uint16')
+            headerLength = headerLength.reshape((1,))[0]
+
+            totalHeaderLength = 10 + headerLength
+
+            arrayFormat = np.fromfile(f, count=headerLength * 1, dtype='S64')
+            arrayFormat = str(arrayFormat.reshape((headerLength, 1))[0])
+            # to interpret the array format infor, we make some faily strict assumptions about its format
+
+            r_descr = re.compile(r"descr....(.\w+)")
+            descr = r_descr.search(arrayFormat)
+            dtNPY = descr.group(1)
+
+            littleEndian = not ('>' in dtNPY)
+
+            for i in range(len(dtypesNPY)):
+                if dtNPY[1:] == dtypesNPY[i]:
+                    dataType = dtypesPYTHON[i]
+
+            r_fortran = re.compile(r"fortran_order...(\w+)")
+            fortran = r_fortran.search(arrayFormat)
+            fortran_sub = fortran.group(1)
+
+            fortranOrder = (fortran_sub == True)
+
+            r_shape = re.compile(r"shape....(\d+)..(\d+)..(\d+)")
+            shape = r_shape.search(arrayFormat)
+            arrayShape = np.int(shape.group(1, 2, 3))
+
+            return arrayShape, dataType, fortranOrder, littleEndian, totalHeaderLength, npyVersion
+
+        except:
+            f.close()
+            print('error')
 
 
 
