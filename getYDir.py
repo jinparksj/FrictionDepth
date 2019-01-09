b2ca462ed4ebc671e0e351f5509c3743ec6b82d0
harrynam <harry_nam@naver.com>
2019-01-08 17:32:13 -0800

    done for YDir

--- getYDir.py
+++ getYDir.py
@@ -13,7 +13,7 @@ def getYDir(N, yDirParam):
 
     y = yDirParam.y0
 
-    for i in range(yDirParam.angleThresh.shape[0])
+    for i in range(yDirParam.angleThresh.shape[0]):
         y = getYDirHelper(N, y, yDirParam.angleThresh[i], yDirParam.iter[i])
 
     return y
@@ -30,4 +30,29 @@ def getYDirHelper(N, y0, thresh, iter):
     # Output:
     #   yDir: the direction of gravity vector as inferred
 
-    nn =
+    nn = np.transpose(N,(2,0,1))
+    nn = nn.reshape((3,nn.size/3))
+    nn = nn[:,(~np.isnan(nn[1,:]))]
+
+    # Set it up as a optimization problem
+
+    yDir = y0;
+    # Let us do hard assignments
+    for i in range(iter):
+        sim0 = np.transpose(yDir)*nn
+        indF = abs(sim0) > np.rad2deg(np.arccos(thresh))
+        indW = abs(sim0) < np.rad2deg(np.arcsin(thresh))
+
+        NF = nn[:,indF]
+        NW = nn[:,indW]
+        A = NW*np.transpose(NW) - NF*np.transpose(NF)
+        b = np.zeros((3,1))
+        c = NF.shape[1]
+
+        V,D = np.linalg.eig(A)
+        gr = min(np.diag(D))
+        ind = np.argmin(np.diag(D))
+        newYDir = V[:,ind]
+        yDir = newYDir*np.sign(np.transpose(yDir)*newYDir)
+
+    return yDir
